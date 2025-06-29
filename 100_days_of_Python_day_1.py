f359bcf0080cb3574afb033382c9d79b16ce8407
tharani2503 <vinothdhanavel2503@gmail.com>
2025-06-30 02:18:57 +0530

    Add files via upload

---
+++ 100_days_of_Python_day_1.py
@@ -0,0 +1,15 @@
+# input function
+print(input("get input from user"))
+
+print("hello"+ input("get world")+"!")
+
+# input("get from user")== whatever we type
+
+# string concatenation
+print("hello"+"world")
+
+print("Welcome to the Band Name Generator")
+city=input("What's the name of the city you grew up in?")
+print(city)
+pet_name=input("What's your pet's name?")
+print("Your band name could be " + city +" "+ pet_name)
